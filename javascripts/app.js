2465fdb03bb74f176fc0cc9bb3ab116cf1c8c35a
Trever Yarrish <trever@defineyouredge.com>
2013-08-12 22:48:59 -0700

    WIP Added form input fields for Feature title, storyGoal, storyStakeholder, storyBehavior
    - TODO: Setup data save and form wrapper
    - added features-list partial and almost completed

--- javascripts/app.js
+++ javascripts/app.js
@@ -14,12 +14,19 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+App.FeaturesRoute = Ember.Route.extend({
+  model: function() {
+    return App.Feature.find();
+  }
+});
+
 App.FeatureRoute = Ember.Route.extend({
 	model: function(params) {
 		return App.Feature.find(params.feature_id);
 	}
 });
 
+App.FeaturesController = Ember.ArrayController.extend();
 App.FeatureController = Ember.ObjectController.extend();
 
 App.Store = DS.Store.extend ({
@@ -28,10 +35,13 @@ App.Store = DS.Store.extend ({
 });
 
 App.Feature = DS.Model.extend({
-  title: DS.attr('title')
+  title: DS.attr('title'),
+  storyGoal: DS.attr('storyGoal'),
+  storyStakeholder: DS.attr('storyStakeholder'),
+  storyBehavior: DS.attr('storyBehavior')
 });
 
 App.Feature.FIXTURES = [
-    {id: 1, title: 'Fixture object 1'},
-    {id: 2, title: 'Fixture object 2'}
+    {id: 1, title: 'Fixture object 1', storyGoal: '', storyStakeholder: '', storyBehavior: ''},
+    {id: 2, title: 'Edit a feature name', storyGoal: 'identify a feature', storyStakeholder: 'user', storyBehavior: 'edit a feature name'}
 ];
