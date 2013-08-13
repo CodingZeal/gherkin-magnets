e223ef23cca6585e3d060603d45323e3572155ea
Sean Culver & Trever Yarrish <git+sean+trever@codingzeal.com>
2013-08-12 17:16:19 -0700

    WIP creating [#50146339]

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,11 @@
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true
+});
 
 App.Router.map(function() {
   // put your routes here
+  this.resource('features');
+  this.resource('feature', {path:'/feature/:feature_id'});
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -9,3 +13,25 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+App.FeatureRoute = Ember.Route.extend({
+	model: function(params) {
+		return App.Feature.find(params.feature_id);
+	}
+});
+
+App.FeatureController = Ember.ObjectController.extend();
+
+App.Store = DS.Store.extend ({
+  revision: 12,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Feature = DS.Model.extend({
+  title: DS.attr('title')
+});
+
+App.Feature.FIXTURES = [
+    {id: 1, title: 'Fixture object 1'},
+    {id: 2, title: 'Fixture object 2'}
+];
